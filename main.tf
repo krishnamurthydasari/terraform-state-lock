a87d51a201cbdb3dbef957f640146d8c162a55af
Krishnamurthy Dasari <krishnamurthydasari@gmail.com>
2021-07-20 16:47:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ provider "aws" {}
 
 terraform {
   backend "s3" {
-    bucket = "aaaaatest"
-    key = "terraform.state"
+    bucket = "terraformbackend"
+    key = "terraform"
+    region = "us-east-2"
     dynamodb_table = "terraform-lock"
   }
 }
